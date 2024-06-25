16cce9e36de122617535282ec7e0d6841f2111c6
Grigoriy Gavrilov <greeb.spb@gmail.com>
2024-06-25 14:24:51 +0500

    add meal item component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
+import { useState } from "react";
 import Header from "./components/Header";
+import { useEffect } from "react";
+import MealItem from "./components/MealItem";
 
 function App() {
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    async function getMeals() {
+      try {
+        const response = await fetch('http://localhost:3000/meals');
+        const data = await response.json();
+        setMeals(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    getMeals();
+  }, [])
   return (
     <>
       <Header />
-      
+      <ul id="meals">
+        {
+          meals.map((meal) => <MealItem meal={meal} />)
+        }
+      </ul>
     </>
   );
 }
---
+++ src/components/MealItem.jsx
@@ -0,0 +1,29 @@
+export default function MealItem({ meal }) {
+  const currencyPrice = new Intl.NumberFormat('en-US',
+    {
+      style: "currency",
+      currency: "EUR"
+    }
+  ).format(meal.price)
+
+  return (
+    <li className="meal-item" key={meal.id}>
+
+      <article>
+        <img src={`http://localhost:3000/${meal.image}`} alt="" />
+        <h3>{meal.name}</h3>
+        <div className="meal-item-price">
+          <span>{currencyPrice}</span>
+        </div>
+        <p className="meal-item-description">
+          {meal.description}
+        </p>
+        <div className="meal-item-actions">
+          <button className="button">Add to Cart</button>
+        </div>
+      </article>
+
+
+    </li>
+  )
+}
