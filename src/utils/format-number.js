08fa40d6cb26a9912fc61522854b4cb30d1179c3
Grigoriy Gavrilov <greeb.spb@gmail.com>
2024-06-27 00:05:38 +0500

    add format function

---
+++ src/utils/format-number.js
@@ -0,0 +1,12 @@
+function formatPrice(num) {
+  const totalFixed = new Intl.NumberFormat('en-US',
+    {
+      style: "currency",
+      currency: "EUR"
+    }
+  ).format(num.toFixed(2));
+
+  return totalFixed;
+}
+
+export default formatPrice; 
